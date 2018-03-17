0f56b74fff83b4afbe59e60e3cdf413fd9084b24
admin <hung506khtn@gmail.com>
2018-03-17 14:53:30 +0700

    pass all test case Leap

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,4 +1,25 @@
+import scala.collection.mutable.ArrayBuffer
+
 object SumOfMultiples {
-  def sum(factors: Set[Int], limit: Int): Int = ???
+  def sum(factors: Set[Int], limit: Int): Int = {
+    val a = factors.map(x => getAllMultipler(x,limit)).flatMap(x => x).toSet
+    val result = if(a.size == 0) 0 else a.foldRight(0){(acc,c) => acc +c}
+    result
+  }
+
+  def getAllMultipler(factor: Int, limit: Int)  = {
+    val buff = new ArrayBuffer[Int]()
+    var value = factor
+    var multi = 1
+    while(value < limit){
+      buff.append(value)
+      multi += 1
+      value = factor * multi
+    }
+    buff.toSet
+  }
+
+
+
 }
 
